7520866e960d0e5bdff139fa581ce2b209cc4369
KhadijaBagani <bagani2000@gmail.com>
2025-04-04 10:56:23 +0200

    queries

--- BDExamen/Recetas/99_muestra_todo.sql
+++ BDExamen/Recetas/99_muestra_todo.sql
@@ -1,4 +1,8 @@
 USE recetas_para_probar;
 
-SELECT*FROM temporadas, categorias, dificultades, recetas, ingredientes, ingredientes_recetas
-
+SELECT*FROM temporadas
+SELECT*FROM  categorias
+SELECT*FROM dificultades
+SELECT*FROM  recetas
+SELECT*FROM  ingredientes
+SELECT*FROM ingredientes_recetas
